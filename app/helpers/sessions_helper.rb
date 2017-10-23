3ebf635c3de49b78574ed819f26980e15bccf4f6
hirofumi maesono <himaesoo@gmail.com>
2017-10-23 11:35:50 +0000

    user login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
-if @current_user
-  return @current_user
-else
-  @current_user = User.find_by(id: session[:user_id])
-  return @current_user
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
