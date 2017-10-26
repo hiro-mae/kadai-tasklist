8f6dc7e4a5220b4cf6ee13187b2bd8fe8be0338f
hirofumi maesono <himaesoo@gmail.com>
2017-10-26 22:38:58 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
   end
 end
