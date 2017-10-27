c245370b4b5ccbb17e548948f5dcc2baa034af66
hirofumi maesono <himaesoo@gmail.com>
2017-10-27 10:17:52 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,7 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     if logged_in?
       @user = current_user
@@ -6,4 +9,53 @@ class TasksController < ApplicationController
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
     end
   end
+  
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+   @task = current_user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to root_url
+    else
+      @tasks =current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :'tasks/index'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_back(fallback_location:root_path) 
+  end
+  
+  private
+  
+  def set_message
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
 end
