d3628b29e615cb5ac69c0aa2ce6a8e81aae12ca9
hirofumi maesono <himaesoo@gmail.com>
2017-10-27 10:36:40 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user
   
   def index
     if logged_in?
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
-    redirect_back(fallback_location:root_path) 
+    redirect_back(fallback_location: root_path) 
   end
   
   private
