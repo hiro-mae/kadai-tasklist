833ac74a9dca3cdbaf6215edc9b75776f50636e9
hirofumi maesono <himaesoo@gmail.com>
2017-11-03 11:05:15 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy, :correct_user]
   
   def index
     if logged_in?
